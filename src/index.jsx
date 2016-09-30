d70c72cde3564248f91227bfc5af733e450d2ec2
Jorge Morales <desarrollador1@cetoco.com>
2016-09-30 08:37:16 -0500

    Minor file structure fix

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './app/App';
-import reducers from './app/rootReducer';
+import reducers from './rootReducer';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
